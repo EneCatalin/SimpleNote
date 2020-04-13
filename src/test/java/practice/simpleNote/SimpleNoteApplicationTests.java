66fda56877075c25bf3fba8cd208e95a26b881a0
Chris <enecatalin9393@gmail.com>
2020-04-13 16:24:18 +0300

    added the test folders

---
+++ src/test/java/practice/simpleNote/SimpleNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.simpleNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
